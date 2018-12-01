b6708bfc0b476c639fdbf62b1c8030181bd88752
Arun A <arun.mudaliar@moonfroglabs.com>
2018-12-02 01:12:40 +0530

    cleanups.

--- UnityProject/Assets/Scripts/Ball.cs
+++ UnityProject/Assets/Scripts/Ball.cs
@@ -4,69 +4,60 @@ using Photon.Pun;
 using UnityEngine;
 
 public class Ball : MonoBehaviourPunCallbacks, IPunObservable {
-    [Tooltip("The local ball instance. Use this to know if the local ball is represented in the Scene")]
-    public static GameObject localBall;
+	[Tooltip("The local ball instance. Use this to know if the local ball is represented in the Scene")]
+	public static GameObject localBall;
 
-    Vector3 initPos = Vector3.zero;
+	Vector3 initPos = Vector3.zero;
 
-    public void Awake() {
-        // #Important
-        // used in GameManager.cs: we keep track of the localPlayer instance to prevent instanciation when levels are synchronized
-        if (photonView.IsMine) {
-            localBall = gameObject;
-        } else {
-            //GetComponent<Rigidbody2D>().isKinematic = true;
-            GetComponent<Rigidbody2D>().simulated = false;
-            GetComponent<CircleCollider2D>().isTrigger = true;
-        }
+	public void Awake() {
+		// used in GameManager.cs: we keep track of the localPlayer instance to prevent instanciation when levels are synchronized
+		if (photonView.IsMine) {
+			localBall = gameObject;
+		} else {
+            // disable the physics simulation for the remote player.  Positions are synched for remote player.
+			GetComponent<Rigidbody2D>().simulated = false;
+			GetComponent<CircleCollider2D>().isTrigger = true;
+		}
 
-        if (PhotonNetwork.IsMasterClient) {
-            if (photonView.IsMine) {
-                GetComponent<SpriteRenderer>().color = Color.red;
-            }
-        } else {
-            if(!photonView.IsMine) {
-                GetComponent<SpriteRenderer>().color = Color.red;
-            }
-        }
+		if (PhotonNetwork.IsMasterClient) {
+			if (photonView.IsMine) {
+				GetComponent<SpriteRenderer>().color = Color.red;
+			}
+		} else {
+			if (!photonView.IsMine) {
+				GetComponent<SpriteRenderer>().color = Color.red;
+			}
+		}
 
-        // #Critical
-        // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
-        DontDestroyOnLoad(gameObject);
-    }
+		// we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+		DontDestroyOnLoad(gameObject);
+	}
 
 	public void Start() {
-        initPos = transform.position;
-        if (photonView.IsMine) {
-            Reset();
-        }
+		initPos = transform.position;
+		if (photonView.IsMine) {
+			Reset();
+		}
 	}
 
 	public void Update() {
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        if (photonView.IsMine) {
-            if (rb.velocity.magnitude<=0.001f) {
-                Reset();
-            }
-        }	
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+		if (photonView.IsMine) {
+            // reset the ball position if the velocity is very less.
+			if (rb.velocity.magnitude <= 0.001f) {
+				Reset();
+			}
+		}
 	}
 
-    void Reset() {
-        if (photonView.IsMine) {
-            transform.SetPositionAndRotation(initPos, Quaternion.identity);
-            GetComponent<Rigidbody2D>().AddForce(Vector2.up * -2.0f + Vector2.left * 10.0f, ForceMode2D.Impulse);
-        }
-    }
+	void Reset() {
+		if (photonView.IsMine) {
+			transform.SetPositionAndRotation(initPos, Quaternion.identity);
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * -2.0f + Vector2.left * 10.0f, ForceMode2D.Impulse);
+		}
+	}
 
-	public void OnPhotonSerializeView( PhotonStream stream, PhotonMessageInfo info ) {
-        if (stream.IsWriting) {
-            // We own this player: send the others our data
-            //stream.SendNext(this.IsFiring);
-            //stream.SendNext(this.Health);
-        } else {
-            // Network player, receive data
-            //this.IsFiring = (bool)stream.ReceiveNext();
-            //this.Health = (float)stream.ReceiveNext();
-        }
-    }
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+        // Not implemented.
+	}
 }
--- UnityProject/Assets/Scripts/Player.cs
+++ UnityProject/Assets/Scripts/Player.cs
@@ -3,71 +3,58 @@ using System.Collections.Generic;
 using Photon.Pun;
 using UnityEngine;
 
-public class Player : MonoBehaviourPunCallbacks, IPunObservable
-{
-    [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
-    public static GameObject localPlayer;
-    public GameObject myBall;
+public class Player : MonoBehaviourPunCallbacks, IPunObservable {
+	[Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+	public static GameObject localPlayer;
+	public GameObject myBall;
 
-    public void Awake()
-    {
-        // #Important
-        // used in GameManager.cs: we keep track of the localPlayer instance to prevent instanciation when levels are synchronized
-        if (photonView.IsMine)
-        {
-            localPlayer = gameObject;
-        }
+	public void Awake() {
+		// used in GameManager.cs: we keep track of the localPlayer instance to prevent instanciation when levels are synchronized
+		if (photonView.IsMine) {
+			localPlayer = gameObject;
+		}
 
-        // #Critical
-        // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
-        DontDestroyOnLoad(gameObject);
-    }
+		// we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+		DontDestroyOnLoad(gameObject);
+	}
 
-    void Update() {
-        if (photonView.IsMine) {
-            float speed = 10.0f;
-            if (Input.GetKey(KeyCode.UpArrow)) {
-                if (transform.position.y < 3.0f) {
-                    transform.position += Vector3.up * speed * Time.deltaTime;
-                } else {
-                    transform.position = new Vector3(transform.position.x, 3.0f, 0.0f);
-                }
-            }
-            if (Input.GetKey(KeyCode.DownArrow)) {
-                transform.position += Vector3.up * -speed * Time.deltaTime;
-                if (transform.position.y > -3.0f) {
-                    transform.position += Vector3.up * -speed * Time.deltaTime;
-                } else {
-                    transform.position = new Vector3(transform.position.x, -3.0f, 0.0f);
-                }
-            }
-        }
-    }
+	void Update() {
+		if (photonView.IsMine) {
+			float speed = 10.0f;
+			if (Input.GetKey(KeyCode.UpArrow)) {
+				// check if stricker reached the top.
+				if (transform.position.y < 3.0f) {
+					transform.position += Vector3.up * speed * Time.deltaTime;
+				} else {
+					// restrict the stricker at top.
+					transform.position = new Vector3(transform.position.x, 3.0f, 0.0f);
+				}
+			}
+			if (Input.GetKey(KeyCode.DownArrow)) {
+				transform.position += Vector3.up * -speed * Time.deltaTime;
+				// check if stricker reached the bottom.
+				if (transform.position.y > -3.0f) {
+					transform.position += Vector3.up * -speed * Time.deltaTime;
+				} else {
+					// restrict the stricker at bottom.
+					transform.position = new Vector3(transform.position.x, -3.0f, 0.0f);
+				}
+			}
+		}
+	}
 
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting)
-        {
-            // We own this player: send the others our data
-            //stream.SendNext(this.IsFiring);
-            //stream.SendNext(this.Health);
-        }
-        else
-        {
-            // Network player, receive data
-            //this.IsFiring = (bool)stream.ReceiveNext();
-            //this.Health = (float)stream.ReceiveNext();
-        }
-    }
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+		// Not implemented.
+	}
 
-    void OnCollisionEnter2D( Collision2D collision ) {
-        //Debug.Log("Collided "+ collision.gameObject.tag);
-        if (collision.gameObject.tag == "ball" && photonView.IsMine) {
-            if (collision.contacts.Length>0) {
-                Vector2 collisionNormal = Vector2.zero;
-                collisionNormal = -collision.contacts[0].normal;
-                collision.rigidbody.AddForce(collisionNormal * 6.0f, ForceMode2D.Impulse);
-            }
-        }
-    }
+	void OnCollisionEnter2D(Collision2D collision) {
+        // if local ball is colliding with stricker (mine) then apply an impulse.
+		if (collision.gameObject.tag == "ball" && photonView.IsMine) {
+			if (collision.contacts.Length > 0) {
+				Vector2 collisionNormal = Vector2.zero;
+				collisionNormal = -collision.contacts[0].normal;
+				collision.rigidbody.AddForce(collisionNormal * 6.0f, ForceMode2D.Impulse);
+			}
+		}
+	}
 }
